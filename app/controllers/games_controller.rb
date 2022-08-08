5a2b3a5b9f5403de9f2ee85928d5385d334408a6
Mark Rawlins <mark.jeffrey.rawlins@gmail.com>
2022-08-08 18:06:49 +0900

    figured out the words included in grid logic in first draft

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,33 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
 
   def new
-    @letters = Array.new(10) { rand(1..9) }
+    # Be AWARE: We don't have access to this variable directly from here!
+    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
   end
 
   def score
+    @letters = params[:letters].split
+    @word = params[:word]
+    @wordLetters = params[:word].split
+    # we need to compare the arrays to see if the letters are in the word
+    @wordLetters.each do |letter|
+      if !@letters.include?(letter)
+        @message = "Sorry, but #{@word} can't be built out of #{@letters}"
+      end
+      p @message
+    end
 
+    url = "https://wagon-dictionary.herokuapp.com/#{@word}"
+    check_validity = URI.open(url).read
+    # Turns a JSON string into a Ruby array of Hashes.
+    @result = JSON.parse(check_validity)
+    if @result
+      @display_result = "#{@word} is valid!"
+    else
+      @display_result = "#{@word} is not valid!"
+    end
   end
 end
